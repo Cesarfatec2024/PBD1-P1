88ffb55c166d94cdb48b387dbd228d87dcc57784
Cesar Galan <cesar.galan@gmail.com>
2024-04-23 10:11:41 -0300

    feat(cursor-unbound): cálculo de preço médio por país

--- P1.sql
+++ P1.sql
@@ -9,3 +9,19 @@ price REAL
 SELECT * FROM tb_wine_reviews;
 
 DROP TABLE tb_wine_reviews;
+
+
+-- 2)
+
+DO $$
+DECLARE
+v_country VARCHAR(100);
+media_price NUMERIC;
+BEGIN
+	FOR v_country IN SELECT DISTINCT country FROM tb_wine_reviews LOOP
+		SELECT AVG(price) INTO media_price FROM tb_wine_reviews WHERE country = v_country;
+		RAISE NOTICE '% : Preço medio = %', v_country, media_price;
+	END LOOP;
+END $$	
+		
+--3)
