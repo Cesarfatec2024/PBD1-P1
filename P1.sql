3c24b9f8048b73a8945e53c4dad5209cb5b50383
Cesar Galan <cesar.galan@gmail.com>
2024-04-23 10:29:21 -0300

    feat(cursor-bound): obtenção de descrição mais longa por país

--- P1.sql
+++ P1.sql
@@ -12,7 +12,6 @@ DROP TABLE tb_wine_reviews;
 
 
 -- 2)
-
 DO $$
 DECLARE
 v_country VARCHAR(100);
@@ -23,5 +22,33 @@ BEGIN
 		RAISE NOTICE '% : Preço medio = %', v_country, media_price;
 	END LOOP;
 END $$	
-		
+
+
 --3)
+DO $$
+DECLARE
+    wine_country VARCHAR(100);
+    max_description TEXT;
+    cur_wines CURSOR FOR
+        SELECT DISTINCT country
+        FROM tb_wine_reviews; 
+BEGIN
+    OPEN cur_wines;
+    FETCH NEXT FROM cur_wines INTO wine_country;
+    WHILE FOUND LOOP
+        SELECT description INTO max_description
+        FROM tb_wine_reviews
+        WHERE country = wine_country
+        ORDER BY LENGTH(description) DESC
+        LIMIT 1;
+
+        RAISE NOTICE 'País - % | Descrição mais longa = %', wine_country, max_description;
+		
+        FETCH NEXT FROM cur_wines INTO wine_country;
+    END LOOP;	
+   CLOSE cur_wines;
+END $$;
+
+
+-- 4)
+
